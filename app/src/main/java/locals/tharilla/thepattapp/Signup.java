08f5e2c2edd49daf290a6a89b624c85846b338ea
Samanyu13 <samanyusaji18@gmail.com>
2018-12-25 20:39:34 +0530

    Setting up login using google - demo

--- app/src/main/java/locals/tharilla/thepattapp/Signup.java
+++ app/src/main/java/locals/tharilla/thepattapp/Signup.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
 
@@ -13,21 +15,27 @@ public class Signup extends AppCompatActivity {
 
     private Firebase mRef;
 
+    private DatabaseReference database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Firebase.setAndroidContext(this);
+
 
         mRef = new Firebase("https://thepattapp51.firebaseio.com/");
 
         creator = (Button)findViewById(R.id.create);
 
+        database = FirebaseDatabase.getInstance().getReference().child("Patt");
+
         creator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Firebase mRefChild = mRef.child("Name");
-                mRefChild.setValue("Soman");
+                Firebase mRefChild = mRef.child("Count");
+                mRefChild.setValue("Raj");
             }
         });
     }
